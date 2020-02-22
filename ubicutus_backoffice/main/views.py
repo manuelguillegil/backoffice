229711b820e3089385b4696fcaa40d12d3eb1a0b
Manuel Guillermo Gil <manuelguillermogil@gmail.com>
2020-02-22 14:17:49 -0400

    Se separó la lógica del base y del home

--- ubicutus_backoffice/main/views.py
+++ ubicutus_backoffice/main/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return render(request,'base_site.html',{'variable':''})
+    return render(request,'home.html',{'variable':''})
 
 def dashboard(request):
     return render(request,'dashboard/index.html',{'variable':''})
