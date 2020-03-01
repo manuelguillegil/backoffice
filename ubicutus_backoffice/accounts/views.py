35b9ccb484708905f76c47e58430814d6f572675
Mafer Mazu <13-10787@usb.ve>
2020-02-29 21:07:03 -0400

    Form de vacacion y avance de envio de correo

--- ubicutus_backoffice/accounts/views.py
+++ ubicutus_backoffice/accounts/views.py
@@ -8,6 +8,8 @@ from django.core.exceptions import ValidationError
 from django.views.decorators.csrf import csrf_protect
 from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.decorators import login_required
+from django.core.mail import BadHeaderError, send_mail
+from django.http import HttpResponse, HttpResponseRedirect
 
 # Create your views here.
 @csrf_protect
@@ -98,3 +100,20 @@ def validate_ubicutus_email(email):
         if (email_reverse[i] != ubic[i]):
             return False
     return True
+
+# Funcion para enviar emails
+# Esto aún tengo que editarlo
+def send_email(request):
+    subject = request.POST.get('subject', '')
+    message = request.POST.get('message', '')
+    from_email = request.POST.get('from_email', '')
+    if subject and message and from_email:
+        try:
+            send_mail(subject, message, from_email, ['admin@example.com'])
+        except BadHeaderError:
+            return HttpResponse('Invalid header found.')
+        return HttpResponseRedirect('/contact/thanks/')
+    else:
+        # In reality we'd use a form class
+        # to get proper validation errors.
+        return HttpResponse('Make sure all fields are entered and valid.')
